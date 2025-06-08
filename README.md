741cb6282f264697530dec9785f0219281a1c348
Abhay Mittal <sirabhaymittal@gmail.com>
2025-06-09 01:07:40 +0530

    Fresh start

---
+++ README.md
@@ -0,0 +1,3 @@
+# E-Commerce-App
+
+-E-Commerce Website made with ReactJS, Node.js
---
+++ backend/server.js
@@ -0,0 +1,36 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './config/mongodb.js'
+import connectCloudinary from './config/cloudinary.js'
+import userRouter from './routes/userRoute.js'
+import productRouter from './routes/productRoute.js'
+// import userModel from './models/userModel.js' // Uncomment if you need to use userModel directly
+// import productModel from './models/productModel.js' // Uncomment if you need to use productModel directly        
+import cookieParser from 'cookie-parser'    
+
+
+
+//App config
+const app=express()
+const port = process.env.PORT || 4000
+connectDB()
+connectCloudinary()
+
+//middleware
+app.use(express.json())
+app.use(cors({
+  origin: 'http://localhost:5173', // your frontend origin
+  credentials: true               // allow cookies
+}));
+//api endpoints
+
+app.use(cookieParser()) // Middleware to parse cookies
+app.use('/api/user',userRouter)
+app.use('/api/product',productRouter)  //where is this /product? 
+
+app.get('/',(req,res)=>{
+    res.send("API Working")
+})
+
+app.listen(port,()=>console.log("Server stared on PORT: "+ port))     //start express server
